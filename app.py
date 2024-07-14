5f2ff9259ef6ab0ad1437013f4a6417c82a936cb
Vishal Tyagi <vt658853@gmail.com>
2024-07-14 17:19:27 +0200

    Samples for air humidity and luminance

--- app.py
+++ app.py
@@ -6,22 +6,24 @@ import plotly.express as px
 
 start_date = datetime.datetime.now()
 end_date = start_date + timedelta(days=10)
-df_temp = pd.read_csv('samples/temp.csv')
+df_temp = pd.read_csv('samples/temp.csv').head(50)
+df_air=pd.read_csv('samples/air_humidity.csv').head(50)
+df_lum=pd.read_csv('samples/luminance.csv').head(50)
 random_date = start_date + (end_date - start_date) * random.random()
 app = Flask(__name__)
 
 def data(col):
     if(col=='temp'):
         return df_temp
-    if(col=='air_humidity'):
+    elif(col=='air_humidity'):
+        return df_air
+    elif(col=='soil_humidity'):
         return df_temp
-    if(col=='soil_humidity'):
+    elif(col=='luminance'):
+        return df_lum
+    elif(col=='water'):
         return df_temp
-    if(col=='luminance'):
-        return df_temp
-    if(col=='water'):
-        return df_temp
-    if(col=='light'):
+    elif(col=='light'):
         return df_temp      
 
 @app.route('/callback', methods=['POST', 'GET']) 
